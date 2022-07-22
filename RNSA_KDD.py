0febf85d99349a7d2056b50c3fe7f937688472f8
izakariyya <zakariyyaidris68@gmail.com>
2022-07-22 12:46:38 +0100

    Add files via upload

---
+++ RNSA_KDD.py
@@ -0,0 +1,147 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 30 09:03:01 2018
+
+@author: User
+"""
+import math
+import random
+import csv
+#import numpy as np
+#import pandas as pd
+#from sklearn.model_selection import train_test_split
+#from sklearn.preprocessing import LabelEncoder
+#from sklearn.ensemble import RandomForestClassifier
+
+def getdata(dataset):
+    with open(dataset, newline = '') as f:
+        rowdata = []
+        reader = csv.reader(f)
+        for row in reader:
+            for i in range(1, len(row)):
+                row[i] = float(row[i])
+            rowdata.append(row)          
+    return rowdata
+
+
+#function for calculating the eucidean distance between two point
+    
+def euclidean_distance(x1, x2):
+    distance = math.sqrt(sum( (x1 - x2)**2 for x1, x2 in zip(x1, x2)))
+    return distance
+
+#Getting the minimum and maximum value for each column
+    
+def dataset_minmax(dataset):
+    minmax = list()
+    for i in range(len(dataset[0])):
+        col_values = [row[i] for row in dataset]
+        value_min = min(col_values)
+        value_max = max(col_values)
+        minmax.append([value_min, value_max])
+    return minmax
+
+# Rescale dataset columns to the range 0-1
+    
+def normalize_dataset(dataset, minmax):
+    normdata = []
+    for row in dataset:
+        for i in range(1, len(row)):
+            row[i] = (row[i] - minmax[i][0]) / (minmax[i][1] - minmax[i][0])
+        normdata.append(row)          
+    return normdata
+
+
+
+#normalized_benign_scan_test = getdata('normalized_scan_benign_test.csv')
+
+
+def generate_random_antibody(norm_train, parameters):
+    #format: [[center], radius]
+    radius = parameters["radius"]
+    center = []
+    for i in range(1,len(norm_train[0])):
+        center.append(random.uniform(0,1))
+    return [center, radius]
+
+
+def generate_detectors(norm_train, population_size, parameters, self_class, non_self_class):
+    antibodies = []
+    original_self_class = [x for x in norm_train if x[0] == self_class]
+    while len(antibodies) < population_size:
+        self_class = original_self_class #this allows the selection above to happen only once
+        proposed_antibody = generate_random_antibody(norm_train,parameters)
+        
+        #select the self class points in each dimension that could be contained in by the proposed antibody
+        for i in range(1,len(self_class[0])):
+            self_class = [s for s in self_class if s[i] >(proposed_antibody[0][i-1]-proposed_antibody[1]) and s[i] <(proposed_antibody[0][i-1]+proposed_antibody[1])]
+#if the self_class list is empty then add the antibody, since there are no points in the self class contained by the hyper-cube containing the hyper-sphere
+
+        if len(self_class) == 0:
+            antibodies.append(proposed_antibody)
+    
+    #check whether the self points selected are actually contained by the hypersphere and not only the hyper cube
+    
+        else:
+            flagged = False
+            for s in self_class:
+                if euclidean_distance(proposed_antibody[0], s[1:]) < proposed_antibody[1]:
+                    flagged = True
+            if flagged == False: #if there are no points that are within the hyper-sphere then add the antibody to the population
+                antibodies.append(proposed_antibody)
+    return antibodies
+
+
+def predict(antibodies, x, self_class, non_self_class):
+    #select the antibodies that could contain the point
+    #for every dimension in the antibody center:
+    
+    for i in range(len(antibodies[0][0])):
+        antibodies = [a for a in antibodies if x[i+1] > (a[0][i]-a[1]) and x[i+1] < (a[0][i]+a[1])]
+        #further filter the set of antibodies
+
+    for a in antibodies:
+        n = len(a[0])
+        if euclidean_distance(a[0], x[1:])*(1.0/n) < a[1]:
+            return non_self_class
+    return self_class
+
+
+#function for calculating accuracy
+    
+def get_accuracy(detectors,test_data, self_class, non_self_class):
+    correct = 0.0
+    incorrect = 0.0
+    
+    for x in test_data:
+        acc = predict(detectors, x, self_class, non_self_class )
+        if x[0] == acc:
+            correct += 1
+            #print("correct")
+        else:
+            incorrect += 1
+            #print("incorrect")
+    accuracy = float(correct) / float(len(test_data))
+    
+    return accuracy
+
+
+
+
+
+#kdd = pd.read_csv('kdd_10.csv')
+
+#Transform_Data = kdd.apply(LabelEncoder().fit_transform)
+
+#Transform_Data.to_csv('Encoded_kdd_10.csv')
+
+#np.savetxt('complt_dt', nusw_data, delimiter =",")
+
+#projected_data = PCA(20).fit_transform(iot_data_small)
+    
+#np.savetxt('PCA_small_IoT', projected_data, delimiter =",")
+
+ 
+#meen_maxee = dataset_minmax(complete_data)
+
+#normalize_data = normalize_dataset(complete_data, meen_maxee)
